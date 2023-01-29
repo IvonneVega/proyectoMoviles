633e9b38a280cf3ea25596b2fa0b76fb0fc7edb7
DESKTOP-IVONNE\Ivonne <ivonne01101@hotmail.com>
2023-01-29 17:38:41 -0500

    Se agregar carpeta views con la pantalla de login

--- proyectoMoviles/App.xaml.cs
+++ proyectoMoviles/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using proyectoMoviles.views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace proyectoMoviles
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Login());
         }
 
         protected override void OnStart()
