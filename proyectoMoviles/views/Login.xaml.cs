5b4f9c6132d8c3123f256cfbc9674746c60545e7
DESKTOP-IVONNE\Ivonne <ivonne01101@hotmail.com>
2023-01-29 20:32:34 -0500

    Se diseña y se da funcionalidad a la pantalla de login

--- proyectoMoviles/views/Login.xaml.cs
+++ proyectoMoviles/views/Login.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using proyectoMoviles.models;
+using SQLite;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,51 @@ namespace proyectoMoviles.views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
+        private SQLiteAsyncConnection con;
         public Login()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            con = DependencyService.Get<Database>().GetConnection();
+        }
+        public static IEnumerable<Persona> Select_where(SQLiteConnection db, string usuario, string contrasena)
+        {
+            return db.Query<Persona>("SELECT * FROM Persona where Usuario=? and Contrasena=?", usuario, contrasena);
+        }
+
+
+        private void loginButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "marvel.db3");
+                var db = new SQLiteConnection(ruta);
+                db.CreateTable<Persona>();
+                IEnumerable<Persona> resultado = Select_where(db, txtUsuario.Text, txtContrasena.Text);
+                if (resultado.Count() > 0)
+                {
+                    //Navigation.PushAsync(new ConsultaRegistro());
+                    DisplayAlert("Alerta", "Ingresa", "Cerrar");
+                }
+                else
+                {
+                    DisplayAlert("Alerta", "Usuario o contraseña incorrecta", "Cerrar");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+                throw;
+            }
+        }
+
+        private void btnRegistro_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registro());
         }
     }
 }
