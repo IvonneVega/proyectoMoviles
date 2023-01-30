70acb7e22f9f6c1b1e3f0bd51a4b3ff51e7c8876
DESKTOP-IVONNE\Ivonne <ivonne01101@hotmail.com>
2023-01-29 20:32:57 -0500

    Se crea el formulario de registro con la funcionalidad

---
+++ proyectoMoviles/views/Registro.xaml.cs
@@ -0,0 +1,44 @@
+﻿using proyectoMoviles.models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace proyectoMoviles.views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Registro : ContentPage
+	{
+        private SQLiteAsyncConnection con;
+        public Registro ()
+		{
+			InitializeComponent ();
+            con = DependencyService.Get<Database>().GetConnection();
+        }
+
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            var datos = new Persona { Nombres = txtNombres.Text, Apellidos=txtApellidos.Text, Edad=Convert.ToInt32(txtEdad.Text),
+                Correo = txtCorreo.Text,Usuario = txtUsuario.Text, Contrasena = txtContrasena.Text };
+            con.InsertAsync(datos);
+            txtNombres.Text = "";
+            txtApellidos.Text = "";
+            txtEdad.Text = "";
+            txtCorreo.Text = "";
+            txtContrasena.Text = "";
+            txtUsuario.Text = "";
+            DisplayAlert("Alerta", "Usuario registrado correctamente", "Ok");
+            Navigation.PushAsync(new Login());
+        }
+
+        private void btnCancelar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Login());
+        }
+    }
+}
