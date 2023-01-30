8902d071b66b35d7cc94fab16c8273da290abecc
DESKTOP-IVONNE\Ivonne <ivonne01101@hotmail.com>
2023-01-29 20:32:06 -0500

    Se crean las clases e interfaz de la base de datos y se cargan las imagenes a utilizar en el login

---
+++ proyectoMoviles.Android/SqliteClient.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using proyectoMoviles.Droid;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+[assembly: Xamarin.Forms.Dependency(typeof(SqliteClient))]
+namespace proyectoMoviles.Droid
+{
+    internal class SqliteClient:Database
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var ruta = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(ruta, "marvel.db3");
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
---
+++ proyectoMoviles/models/Persona.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proyectoMoviles.models
+{
+    public class Persona
+    {
+        [PrimaryKey, AutoIncrement] 
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string Nombres { get; set; }
+        [MaxLength(255)]
+        public string Apellidos { get; set; }
+        [MaxLength(255)]
+        public string Correo { get; set; }
+        [MaxLength(255)]
+        public int Edad { get; set; }
+        [MaxLength(255)]
+        public string Usuario { get; set; }
+        [MaxLength(255)]
+        public string Contrasena { get; set; }
+
+    }
+}
